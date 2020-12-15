ae2c49e88497dc0aca08c402af726c040f45e60b
Josh Everett <josheverett93@gmail.com>
2020-12-15 13:27:44 -0600

    error displaying in redux store

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,6 @@
 import { SIGN_IN, BASE_API_URL } from '../utils/constants';
 import axios from 'axios';
+import { getErrors } from './errors';
 
 export const signIn = (user) => ({
     type: SIGN_IN,
@@ -17,7 +18,8 @@ export const initiateLogin = (email, password) => {
             localStorage.setItem('user_token', user.token);
             dispatch(signIn(user));
         } catch (error) {
-            console.log(error);
+            console.log('error', error);
+            error.response && dispatch(getErrors(error.response.data));
         }
     };
 };
@@ -29,6 +31,7 @@ export const registerNewUser = (data) => {
             return { success: true };
         } catch (error) {
             console.log('error', error);
+            error.response && dispatch(getErrors(error.response.data));
             return { success: false };
         }
     };
