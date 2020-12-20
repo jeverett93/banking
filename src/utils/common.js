63c8035f2e4ef1793573bb08f1df19dcb8db5cfe
Josh Everett <josheverett93@gmail.com>
2020-12-20 13:04:14 -0600

    added default authorization header

--- src/utils/common.js
+++ src/utils/common.js
@@ -3,6 +3,7 @@ import store from '../store/store';
 import { initiateGetProfile } from '../actions/profile';
 import { signIn } from '../actions/auth';
 import { history } from '../router/AppRouter';
+import axios from 'axios';
 
 export const validateFields = (fieldsToValidate) => {
     return fieldsToValidate.every((field) => Object.values(field)[0] !== '');
@@ -33,3 +34,14 @@ export const updateStore = (user) => {
     );
     store.dispatch(initiateGetProfile(email));
 };
+
+export const setAuthHeader = () => {
+    const token = localStorage.getItem('user_token');
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    }
+};
+
+export const removeAuthHeader = () => {
+    delete axios.defaults.headers.common['Authorization']
+}
