506b69923996c0934a9e4e1be19f3ff3d5c18cec
Josh Everett <josheverett93@gmail.com>
2020-12-23 19:36:13 -0600

    edited a components conditionally

--- src/components/AccountForm.js
+++ src/components/AccountForm.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
+import _ from 'lodash';
+import {
+    initiateGetAccntDetails,
+    initiateAddAccntDetails,
+    initiateUpdateAccntDetails
+} from '../actions/account';
+import {
+    initiateWithdrawAmount,
+    initiateDepositAmount
+} from '../actions/transactions';
+import { resetErrors } from '../actions/errors';
 import { validateFields } from '../utils/common';
+import { maskNumber } from '../utils/mask';
+import AddAccountForm from './AddAcountForm';
 
 class AccountForm extends React.Component {
     state = {
         amount: '',
+        account: this.props.account,
         editAccount: false,
-        account: {},
         ifsc: '',
         errorMsg: ''
     };
+
+    componentDidMount() {
+        const { email } = this.props;
+        if (email) {
+            this.props.dispatch(initiateGetAccntDetails());
+        }
+    }
+
+    componentWillUnmount() {
+        this.props.dispatch(resetErrors());
+    }
+
+    componentDidUpdate(prevProps) {
+        if (!_.isEqual(prevProps.account, this.props.account)) {
+            this.setState({ account: this.props.account });
+        }
+        if (!_.isEqual(prevProps.errors, this.props.errors)) {
+            this.setState({ errorMsg: this.props.errors });
+        }
+    }
     
     handleUpdateAccount = (ifsc) => {
         const fieldsToValidate = [{ ifsc }];
@@ -26,6 +59,7 @@ class AccountForm extends React.Component {
             this.setState({
                 errorMsg: ''
             });
+            this.props.dispatch(initiateUpdateAccntDetails(ifsc));
         }
     };
 
@@ -44,13 +78,63 @@ class AccountForm extends React.Component {
         });
     };
 
+    handleOnSubmit = (event) => {
+        event.preventDefault();
+        let {  amount, account } = this.state;
+
+        const { selectedType } = this.props;
+        const fieldsToValidate = [{ amount }];
+
+        const allFieldsEntered = validateFields(fieldsToValidate);
+        if (!allFieldsEntered) {
+            this.setState({
+                errorMsg: {
+                    [selectedType === 'withdraw'
+                ? 'withdraw_error'
+            : 'add_error']: 'Please enter an ammount.'
+                }
+            });
+        } else {
+            let { total_balance } = account;
+            amount = +amount;
+            total_balance = +total_balance;
+            if (selectedType === 'withdraw' && amount <= total_balance) {
+                this.props.dispatch(initiateWithdrawAmount(account.account_id, amount));
+                this.setState({
+                    errorMsg: ''
+                });
+            } else if (selectedType === 'deposit') {
+                this.props.dispatch(initiateDepositAmount(account.account_id, amount));
+                this.setState({
+                    errorMsg: ''
+                });
+            } else {
+                this.setState({
+                    errorMsg: {
+                        [selectedType === 'withdraw'
+                        ? 'withdraw_error'
+                    : 'add_error']: "You don't have enough balance in your account"
+                    }
+                });
+            }
+        }
+    };
+
+    handleAddAccount = (account) => {
+        const { account_no, bank_name, ifsc } = account;
+        this.props
+            .dispatch(initiateAddAccntDetails(account_no, bank_name, ifsc))
+            .then(() => this.props.dispatch(initiateAddAccntDetails()));
+    };
+
     render() {
         const { selectedType } = this.props;
-        const { editAccount, ifsc, errorMsg, account = '' } = this.state;
-        const account_no = account.account_no ? account.account_no: '';
+        const { editAccount, ifsc, errorMsg, account } = this.state;
+        const account_no = account.account_no ? maskNumber(account.account_no) : '';
         const type = selectedType.charAt(0).toUpperCase() + selectedType.slice(1)
 
-        return editAccount ? (
+        return account_no ? (
+            editAccount ? (
            <div className="edit-account-form col-md-6 offset-md-3">
                <h3>
                    Account details
@@ -96,6 +180,9 @@ class AccountForm extends React.Component {
         ) : (
             <div className="account-form col-md-6 offset-md-3">
                 {errorMsg && errorMsg.withdraw_error && (
+                    <p className="errorMsg">{errorMsg.withdraw_error}</p>
+                )}
+                {errorMsg && errorMsg.add_error && (
                     <p className="errorMsg">{errorMsg.add_error}</p>
                 )}
                 <Form onSubmit={this.handleOnSubmit} className="account-form">
@@ -113,10 +200,14 @@ class AccountForm extends React.Component {
                     <Form.Group controlId="accnt_no">
                         <Form.Label>Account Number: </Form.Label>
                         <span className="label-value">
-                            {account && account.total_balance}
+                            {account && account_no}
                         </span>
                     </Form.Group>
-                    <Form.Group>
+                    <Form.Group controlId="accnt_no">
+                        <Form.Label>Available Balance: </Form.Label>
+                        <span className="label-value">{account && account.total_balance}</span>
+                    </Form.Group>
+                    <Form.Group controlId="amount">
                         <Form.Label>Amount:</Form.Label>
                         <Form.Control
                             type="number"
@@ -130,8 +221,17 @@ class AccountForm extends React.Component {
                     </Button>
                 </Form>
             </div>
+            )
+        ) : (
+            <AddAccountForm handleAddAccount={this.handleAddAccount} />
         );
     }
 }
 
-export default connect()(AccountForm);
+const mapStateToProps = (state) => ({
+    email: state.auth && state.auth.email,
+    account: state.account,
+    errors: state.errors
+});
+
+export default connect(mapStateToProps)(AccountForm);
