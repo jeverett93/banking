5de8109e991ceb356375d54707e7fb8958a4a35a
Josh Everett <josheverett93@gmail.com>
2020-12-29 20:43:47 -0600

    created route for getting account info

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const authRoute = require('./routes/auth');
 const profileRoute = require('./routes/profile');
+const accountRoute = require('./routes/account');
 require('dotenv').config();
 
 const app = express();
@@ -10,7 +11,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use(authRoute);
-app.use(profileRoute)
+app.use(accountRoute.Router);
+app.use(profileRoute);
 
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
