78fe6255c0a082a839b1a5e1f84b4464ad6c4f11
Josh Everett <josheverett93@gmail.com>
2021-01-05 12:23:14 -0600

    created report component

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import AppRouter from './router/AppRouter';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './css/main.css' 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+import './css/main.css'
+import { maintainSession } from './utils/common';
 
+maintainSession();
 const rootElement = document.getElementById('root')
 
 ReactDOM.render(
