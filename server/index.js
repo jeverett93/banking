0e6eb741f77996c7de1a4955bc6c1440e8ee6922
Josh Everett <josheverett93@gmail.com>
2021-01-06 18:42:03 -0600

    using ejs templating engine

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ const cors = require('cors');
 const authRoute = require('./routes/auth');
 const profileRoute = require('./routes/profile');
 const accountRoute = require('./routes/account');
-const transactionRoute = require('./routes/transations');
+const transactionRoute = require('./routes/transactions');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.set('view engine', 'ejs');
 app.use(cors());
 app.use(authRoute);
 app.use(accountRoute.Router);
