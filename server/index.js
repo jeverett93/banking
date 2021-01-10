fb27822d38a27dc4d534e89738672b7aac14f216
Josh Everett <josheverett93@gmail.com>
2021-01-09 20:42:07 -0600

    troubleshooting heroku issue

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require("path");
 const authRoute = require('./routes/auth');
 const profileRoute = require('./routes/profile');
 const accountRoute = require('./routes/account');
@@ -8,6 +9,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
@@ -17,6 +19,10 @@ app.use(authRoute);
 app.use(accountRoute.Router);
 app.use(profileRoute);
 app.use(transactionsRoute);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
