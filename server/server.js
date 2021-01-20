64c08c5cd9d800460460dd6060abb8e4a1224a38
Josh Everett <josheverett93@gmail.com>
2021-01-19 19:52:03 -0600

    restructured connection

--- server/server.js
+++ server/server.js
@@ -15,9 +15,10 @@ app.use(authRoute);
 app.use(accountRoute.Router);
 app.use(profileRoute);
 app.use(transactionsRoute);
+app.use(express.static(path.join(__dirname, "../build")))
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
 app.listen(PORT, () => {
